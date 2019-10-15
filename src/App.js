39b08b06100cf6c31306a185388fa559db6f645f
cszukala <45420006+cszukala@users.noreply.github.com>
2019-10-15 15:26:49 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,220 @@
+import React, { Component } from 'react';
+import './App.css';
+import 'ol/ol.css'
+import { Map, View } from 'ol'
+import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer'
+import {TileJSON} from 'ol/source'
+import Point from 'ol/geom/Point'
+import { fromLonLat } from 'ol/proj'
+import {Circle as CircleStyle, Icon, Style, Fill, Stroke} from 'ol/style.js';
+import { circular as circularPolygon } from 'ol/geom/Polygon.js'
+import Feature from 'ol/Feature';
+import VectorSource from 'ol/source/Vector';
+import image from './data/icon.png'
+import image2 from './data/ship.png'
+import TextFields from './formcomponent'
+import CallerFields from './formcomponent2'
+import servername from './const'
+
+
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+
+  },
+  dense: {
+    marginTop: 19,
+  },
+  menu: {
+    width: 200,
+  },
+});
+class App extends Component {
+  state = {
+    rffs: [[-87.921045, 30.238719], [-87.899646, 30.562203], [-88.107891, 30.418700]],
+    callers: [],
+    center: [-87.921045, 30.458719],
+    features: [],
+    markers: [],
+    currentValue: ''
+  }
+  rffMarker1 = new Feature({
+    // type: 'icon',
+    information: "rffMarker1",
+    geometry: new Point(fromLonLat(this.state.rffs[0])),
+  })
+  rffMarker2 = new Feature({
+    // type: 'icon',
+    information: "rffMarker2",
+    geometry: new Point(fromLonLat(this.state.rffs[1])),
+  })
+  rffMarker3 = new Feature({
+    // type: 'icon',
+    information: "rffMarker3",
+    geometry: new Point(fromLonLat(this.state.rffs[2])),
+  })
+  
+  imageStyle = new Style({
+    image: new Icon({
+      src: image,
+      // the scale factor
+      scale: .06,
+      anchor: [0.5, 0.5],
+      anchorXUnits: 'fraction',
+      anchorYUnits: 'fraction',
+    })
+  })
+
+  iconStyle = this.imageStyle
+
+  vectorSource = new VectorSource({
+    features: [this.rffMarker1, this.rffMarker2, this.rffMarker3]
+  })
+  vectorLayer = new VectorLayer({
+    source: this.vectorSource,
+    renderBuffer: 200
+  })
+
+  vectorAlerts = new VectorSource({
+    features: this.state.features
+  })
+  vectorAlertLayer = new VectorLayer({
+    source:this.vectorAlerts
+  })
+  olmap = new Map({
+    layers: [
+      new TileLayer({
+        source: new TileJSON({
+          url: 'https://api.maptiler.com/maps/darkmatter/tiles.json?key=XrzPgKMizSI556imElBQ',
+          tileSize: 512,
+          crossOrigin: 'anonymous'
+        })
+      }),
+      this.vectorAlertLayer,
+      this.vectorLayer,
+    ],
+    view: new View({
+      center: fromLonLat(this.state.center),
+      zoom: 10
+    })
+  })
+  doesFeatureExist(longitude, latitude) {
+    for(var i = 0; i < this.state.rffs.length; i++)
+    {
+      if(this.state.rffs[i][0] == latitude && this.state.rffs[i][1] == longitude)
+      {
+        return true;
+      }
+    }
+    return false
+  }
+  // Study arrow syntax and callbacks if you don't understand the fetch request belowl
+  getJsonFromServer() {
+    fetch(`${servername}/getrff`)
+    .then(res => res.json())
+    .then(data=>{
+      if(data != null) {
+        for(let element of data) {
+          //console.log(element)
+          if(element.rff_name != null && !this.doesFeatureExist(parseFloat(element.lat), parseFloat(element.lon))) {
+              //console.log(element.rff_name)
+              //console.log(this.state.rffs)
+              this.createRFF(parseFloat(element.lon), parseFloat(element.lat), element.rff_name)
+              //console.log(this.state.rffs)
+            }
+        }
+        }
+      }
+    )
+  }
+
+  showInfo(event) {
+    var features = this.olmap.getFeaturesAtPixel(event.pixel);
+    if (!features) {
+      this.setState({
+        currentFeatureText: ''
+      })
+      return;
+    }
+    var properties = features[0].getProperties();
+    console.log(properties.information)
+  }
+  createRFF(lat, long, name) {
+    console.log(lat, long, name)
+    var rff = [lat, long]
+    this.state.rffs.push(rff);
+    var rffMarker = new Feature({
+      // type: 'icon',
+      information:  name,
+      geometry: new Point(fromLonLat([lat, long])),
+    })
+    this.vectorSource.addFeature(rffMarker)
+    this.vectorLayer.source = this.vectorSource
+    rffMarker.setStyle(this.iconStyle)
+    
+    this.vectorAlertLayer.getSource().addFeature(new Feature(circularPolygon([lat, long], 20000, 64).clone().transform('EPSG:4326', 'EPSG:3857')))
+  }
+  createCaller(lat, long, name) {
+    console.log(lat, long, name)
+    var newCaller = [lat, long]
+    this.state.callers.push(newCaller);
+    var callermarker = new Feature({
+      // type: 'icon',
+      information:  name,
+      geometry: new Point(fromLonLat([lat, long])),
+    })
+    this.vectorSource.addFeature(callermarker)
+    this.vectorLayer.source = this.vectorSource
+    callermarker.setStyle(new Style({
+      image: new Icon({
+        src: image2,
+        // the scale factor
+        scale: 1,
+        anchor: [0.5, 0.5],
+        anchorXUnits: 'fraction',
+        anchorYUnits: 'fraction',
+      })
+    }))
+  }
+  componentDidMount() {
+    this.rffMarker1.setStyle(this.iconStyle)
+    this.rffMarker2.setStyle(this.iconStyle)
+    this.rffMarker3.setStyle(this.iconStyle)
+    this.olmap.setTarget('map')
+    this.olmap.renderSync()
+    this.olmap.on('click', this.showInfo.bind(this))
+    // this.vectorAlerts.on('addfeature', this.flash.bind(this))
+    let radius = 20000
+    let edgeCount = 64
+
+    for (var i = 0; i < 3; i++)
+    {
+      this.vectorAlertLayer.getSource().addFeature(new Feature(circularPolygon(this.state.rffs[i], radius, edgeCount).clone().transform('EPSG:4326', 'EPSG:3857')))
+    }
+    setInterval(()=>this.getJsonFromServer(), 2000)
+    // console.log('This is the servername', servername)
+  }
+
+  render() {
+    const { classes } = this.props 
+    return (
+      <div className="App">
+        <div id="sidebox" className="omni">
+            <button id="menu" title="Open Menu"></button>
+            <TextFields rffadd={this.createRFF.bind(this)}></TextFields>
+            <CallerFields calleradd={this.createCaller.bind(this)}></CallerFields>
+            <button id="LoB" title="Calculate LoB"></button>
+          </div> 
+        <div id="map" className="map"></div>
+        <pre id="info">{this.state.currentFeatureText}</pre>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/formcomponent.js
@@ -0,0 +1,142 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import servername from './const'
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  button: {
+    margin: theme.spacing.unit,
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+  },
+  dense: {
+    marginTop: 19,
+  },
+  menu: {
+    width: 200,
+  },
+});
+
+class TextFields extends React.Component {
+  state = {
+    longitude: 0,
+    latitude: 0,
+    name: '',
+    open: false,
+  };
+  handleOpen = () => {
+    this.setState({open: true});
+  };
+
+  handleClose = () => {
+    this.setState({open: false});
+  };
+
+  handleChange = name => event => {
+    this.setState({ [name]: event.target.value });
+   
+  };
+  
+  handlePostRequest() {
+    const data = {
+      rff_name: this.state.name,
+      lat: this.state.latitude,
+      lon: this.state.longitude,
+    }
+    console.log(data)
+    console.log(JSON.stringify(data, null, 2))
+    fetch(`${servername}/submit`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data, null, 2)
+    }).then(res => console.log(res))
+  }
+  addrff = () => {
+    this.handlePostRequest();
+    this.props.rffadd(this.state.latitude, this.state.longitude, this.state.name);
+    this.setState({open: false});
+  }
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <div>
+      <button id="rff" onClick={this.handleOpen} title="Add RFF"></button>
+      <Dialog open={this.state.open} onClose={this.handleClose}>
+      <DialogTitle id="form-dialog-title">Add Request for Forces</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          Enter Corresponding data for RFF it will be displayed. Make sure the information is correct before pressing submit.
+        </DialogContentText>
+        <TextField
+          id="rff name"
+          label="rff-name"
+          className={classes.textField}
+          value={this.state.name}
+          onChange={this.handleChange('name')}
+          margin="normal"
+          fullWidth
+        />
+
+        <TextField
+        id="standard-df"
+        label="latitude"
+        value={this.state.latitude}
+        onChange={this.handleChange('latitude')}
+        type="number"
+        className={classes.textField}
+        InputLabelProps={{
+            shrink: true,
+        }}
+        margin="normal"
+        fullWidth
+        />
+
+        <TextField
+          id="standard-df"
+          label="longitude"
+          value={this.state.longitude}
+          onChange={this.handleChange('longitude')}
+          type="number"
+          className={classes.textField}
+          InputLabelProps={{
+            shrink: true,
+          }}
+          margin="normal"
+          fullWidth
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={this.handleClose} color="primary">
+          Cancel
+        </Button>
+        <Button variant="contained" color="primary" className={classes.button} onClick={this.addrff}>
+          Add
+        </Button>
+      </DialogActions>
+      </Dialog>
+      </div>
+    );
+  }
+}
+
+TextFields.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(TextFields);
