259a164e7b606826f1f5511108ddbd470e93e2ab
Matthew Wong <matthew@thewongcode.net>
2019-11-18 14:21:34 -0700

    Update const.js

--- src/const.js
+++ src/const.js
@@ -1,3 +1,3 @@
-const servername = 'http://ec2-18-220-215-42.us-east-2.compute.amazonaws.com:3000'
+const servername = 'http://ec2-34-220-147-201.us-west-2.compute.amazonaws.com:3000'
 
 export default servername
